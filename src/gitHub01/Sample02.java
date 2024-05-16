ea635bdb07e7ac953cb1edb298fa79529358f2cb
hnawg <hnawg@VICTUS>
2024-05-16 14:55:25 +0530

    [Sanghpal] --> update 1st repository

---
+++ src/gitHub01/Sample02.java
@@ -0,0 +1,11 @@
+package gitHub01;
+
+public class Sample02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello github 001");
+
+	}
+
+}
